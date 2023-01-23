7ee7d425dfe2d7b71b4b93f8655695a9710e2494
Kompan <112909122+AnnaKompan@users.noreply.github.com>
2023-01-23 15:35:37 +0200

    modified

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 const gallery = document.querySelector(".gallery");
 const galleryItem = createGalleryItems(galleryItems);
 
@@ -26,34 +23,17 @@ function createGalleryItems(items) {
 
 gallery.innerHTML = galleryItem;
 
-// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
-
-// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-
 gallery.addEventListener("click", bigImgURLGetter);
-const bigImgUrl = bigImgURLGetter;
-
-// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+const bigImgUrl = bigImgURLGetter();
 
 function bigImgURLGetter(evt) {
   evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
   const bigImg = basicLightbox.create(`<img
   src=${evt.target.dataset.source} width="800" height="600">`);
   bigImg.show();
-}
-// 6.Додай закриття модального вікна після натискання клавіші Escape.
-// Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
-// Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
-
-onShow: (bigImg) => {
-  document.onkeydown = function (esc) {
-    if (esc.code === "Escape") {
+  gallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
       bigImg.close();
-      console.log(esc.code);
     }
-  };
-};
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Використовуй готовий код з першого завдання.
 const gallery = document.querySelector(".gallery");
 const galleryItem = createGalleryItems(galleryItems);
 
@@ -17,22 +13,10 @@ function createGalleryItems(items) {
     )
     .join("");
 }
-// 2. Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs.
-// Необхідно додати посилання на два файли: simple - lightbox.min.js і simple - lightbox.min.css.
-// 3. Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
-// Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
 
 gallery.innerHTML = galleryItem;
 
-gallery.addEventListener("click", onClick);
-function onClick(evt) {
-  evt.preventDefault();
-}
-
-// 4. Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
-// Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
-
-const lightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionPosition: "bottom",
   captionsDelay: 250,
